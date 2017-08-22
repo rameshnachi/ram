9c99e12069b283043be764d5f8fc245daae45f01
rameshnachi <30927384+rameshnachi@users.noreply.github.com>
2017-08-22 16:09:07 +0530

    Create ramesh.c

---
+++ ramesh.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int a;
+ scanf("%d",&a);
+ printf("\n ENTER THE NUMBER");
+ if(a>0)
+ {
+  printf("\n positive number");
+  }
+  else(a<0)
+  {
+  printf("\n negative number");
+  }
+  else(a==0)
+  {
+  printf("\n zero")
+  }
+  getch();
+  }
